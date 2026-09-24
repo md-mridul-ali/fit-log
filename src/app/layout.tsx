6f5176a4c49a1c920d3d3b38ec339429a4c7790a
md-mridul-ali <mridulaiub17@gmail.com>
2026-09-24 14:30:47 +0600

    Project setup and added resources

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,11 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <main className="mx-auto w-full max-w-[1380px]">
+            {children}
+        </main>
+      </body>
     </html>
   );
 }
